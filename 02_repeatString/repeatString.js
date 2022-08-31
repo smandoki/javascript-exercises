c2e6c1b1d84aa4d61cd5cb10f23b9e7210da366a
smandoki <80867148+smandoki@users.noreply.github.com>
2022-08-31 11:00:21 +0800

    Fixed exercise 02_repeatString
    Implemented repeat string function and passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n < 0)
+        return 'ERROR';
 
+    output = '';
+
+    for (let i = 0; i < n; i++){
+        output += str;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
