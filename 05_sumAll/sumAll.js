dd8316b015ca940b8dfa0c0301a1ca618ac2df04
smandoki <80867148+smandoki@users.noreply.github.com>
2022-08-31 11:50:07 +0800

    Completed exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number')
+        return 'ERROR';
 
+    let sum = 0;
+
+    const min = num1 < num2 ? num1 : num2;
+    const max = num1 > num2 ? num1 : num2;
+
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
